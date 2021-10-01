d165720ad71797fcb9b797c311a7a64900a7bc00
Walentin <valession@gmail.com>
2021-10-01 13:00:07 +0300

    Оптимизация программной структуры приложения

--- src/com/application/Main.java
+++ src/com/application/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("scenes/admin_workspace.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("scenes/welcome_scene.fxml"));
         primaryStage.setTitle("Учет услуг ветеринарной клиники");
         primaryStage.setScene(new Scene(root));
         primaryStage.setMaximized(false);
---
+++ src/com/application/scenes/ControllerGetToDoctor.java
@@ -0,0 +1,17 @@
+package com.application.scenes;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControllerGetToDoctor {
+
+    @FXML
+    private Button getDoctorButton;
+
+
+    @FXML
+    void initialize(){
+
+    }
+
+}
